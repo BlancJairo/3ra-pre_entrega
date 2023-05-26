1e2843dc5353eb43d54effcb753a608d02bfcafd
BlancJairo <jairoexeblanc@gmail.com>
2023-05-26 19:44:24 -0300

    agregué la app perfiles, quite motos y bicicletas

--- app_preentrega/models.py
+++ app_preentrega/models.py
@@ -9,8 +9,7 @@ class autos (models.Model):
     descripcion = models.CharField(max_length=200)
     
     def __str__(self):
-        return f"{self.marca}, {self.modelo}, {self.ano}"
-    
+        return f"{self.marca}, {self.modelo}, {self.ano}"  
 class Camionetas (models.Model):
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
@@ -32,17 +31,6 @@ class Camiones (models.Model):
     def __str__(self):
         return f"{self.marca}, {self.modelo}, {self.ano}"
 
-class motos (models.Model):
-    marca = models.CharField(max_length=50)
-    modelo = models.CharField(max_length=50)
-    ano = models.CharField(max_length=4)
-    color = models.CharField(max_length=15)
-    equipamiento = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=200)
-
-    def __str__(self):
-        return f"{self.marca}, {self.modelo}, {self.ano}"
-class bicicletas (models.Model):
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
     ano = models.CharField(max_length=4)
